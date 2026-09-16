a4d781fcc42ea951b2f2efdf2b11b70759777877
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2026-09-16 20:55:54 -0300

    feat(quiz_de_transito): criado activity_quiz_de_transito.xml e header

--- app/src/main/java/com/example/layout_telas/QuizDeTransito.java
+++ app/src/main/java/com/example/layout_telas/QuizDeTransito.java
@@ -1,9 +1,6 @@
 package com.example.layout_telas;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -12,13 +9,13 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
-    Button btnProximoLayout;
+public class QuizDeTransito extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_quiz_de_transito);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -26,12 +23,5 @@ public class MainActivity extends AppCompatActivity {
         });
         // Muda as cores das barras de status
         WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setAppearanceLightStatusBars(false);
-
-        btnProximoLayout = findViewById(R.id.btnProximoLayout);
-    }
-
-    public void setBtnProximoLayout(View view) {
-        Intent it = new Intent(getApplicationContext(), QuizDeTransito.class);
-        startActivity(it);
     }
 }
