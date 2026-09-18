842f30aad9a980a926af5d43adb204faa55465d2
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2026-09-17 21:37:10 -0300

    feat(QuizDeTransito): feito definição das variáveis e adicionado botão de voltar no layout (e onclick junto)

--- app/src/main/java/com/example/layout_telas/QuizDeTransito.java
+++ app/src/main/java/com/example/layout_telas/QuizDeTransito.java
@@ -1,6 +1,10 @@
 package com.example.layout_telas;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,7 +14,9 @@ import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class QuizDeTransito extends AppCompatActivity {
-
+    TextView txtCurrentQuestion, txtQuestion;
+    ImageView imgPlacaTransito;
+    Button btnOption1, btnOption2, btnOption3, btnOption4, btnProximaPergunta, btnProximoLayout, btnVoltar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -23,5 +29,25 @@ public class QuizDeTransito extends AppCompatActivity {
         });
         // Muda as cores das barras de status
         WindowCompat.getInsetsController(getWindow(), getWindow().getDecorView()).setAppearanceLightStatusBars(false);
+
+        // Definição de variáveis do layout
+        txtCurrentQuestion = findViewById(R.id.txtCurrentQuestion);
+        txtQuestion = findViewById(R.id.txtQuestion);
+        imgPlacaTransito = findViewById(R.id.imgPlacaTransito);
+        btnOption1 = findViewById(R.id.btnOption1);
+        btnOption2 = findViewById(R.id.btnOption2);
+        btnOption3 = findViewById(R.id.btnOption3);
+        btnOption4 = findViewById(R.id.btnOption4);
+        btnProximaPergunta = findViewById(R.id.btnProximaPergunta);
+        btnProximoLayout = findViewById(R.id.btnProximoLayout);
+        btnVoltar = findViewById(R.id.btnVoltar);
+    }
+
+    public void setBtnProximoLayout(View view) {
+        // a fazer
+    }
+
+    public void setBtnVoltar(View view) {
+        finish();
     }
 }
