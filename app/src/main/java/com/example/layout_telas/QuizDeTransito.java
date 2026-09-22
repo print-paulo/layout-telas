0d5f98b0d9285ff966fdd79cfc72492e77876177
Paulo Henrique <paulohenriquenscmnt06@gmail.com>
2026-09-22 19:27:46 -0300

    feat: feito montarPergunta() e classe de pergunta

--- app/src/main/java/com/example/layout_telas/QuizDeTransito.java
+++ app/src/main/java/com/example/layout_telas/QuizDeTransito.java
@@ -13,10 +13,33 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class QuizDeTransito extends AppCompatActivity {
-    TextView txtCurrentQuestion, txtQuestion;
+    // Modelo de pergunta
+    private static class Pergunta {
+        String[] alternativas;
+        int indiceCorreta; // 0 a 3
+
+        Pergunta(String[] alternativas, int indiceCorreta) {
+            this.alternativas = alternativas;
+            this.indiceCorreta = indiceCorreta;
+        }
+    }
+
+    // Views
+    TextView txtCurrentQuestion;
     ImageView imgPlacaTransito;
-    Button btnOption1, btnOption2, btnOption3, btnOption4, btnProximaPergunta, btnProximoLayout, btnVoltar;
+    Button[] botoesAlternativa;
+    Button btnProximaPergunta, btnProximoLayout, btnVoltar;
+
+    // Estado do Quiz
+    private List<Pergunta> perguntas = new ArrayList<>();
+    private int perguntaAtual = 0;
+    private int indiceSelecionado = -1; // qual botão tá selecionado na hora
+    private boolean respondida = false;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -30,18 +53,46 @@ public class QuizDeTransito extends AppCompatActivity {
             return insets;
         });
 
-
         // Definição de variáveis do layout
         txtCurrentQuestion = findViewById(R.id.txtCurrentQuestion);
-        txtQuestion = findViewById(R.id.txtQuestion);
         imgPlacaTransito = findViewById(R.id.imgPlacaTransito);
-        btnOption1 = findViewById(R.id.btnOption1);
-        btnOption2 = findViewById(R.id.btnOption2);
-        btnOption3 = findViewById(R.id.btnOption3);
-        btnOption4 = findViewById(R.id.btnOption4);
+        botoesAlternativa = new Button[]{
+                findViewById(R.id.btnOption1),
+                findViewById(R.id.btnOption2),
+                findViewById(R.id.btnOption3),
+                findViewById(R.id.btnOption4)
+        };
         btnProximaPergunta = findViewById(R.id.btnProximaPergunta);
         btnProximoLayout = findViewById(R.id.btnProximoLayout);
         btnVoltar = findViewById(R.id.btnVoltar);
+    }
+
+    private void montarPergunta() {
+        // Pergunta placa de Proibido Estacionar
+        perguntas.add(new Pergunta(
+                new String[]{"PCurva Acentuada a Esquerda", "Sentido Proibido", "Proibido Estacionar","Velocidade Máxima"},
+                2
+        ));
+        // Pergunta placa de Curva Acentuada a Esquerda
+        perguntas.add(new Pergunta(
+                new String[]{"Proibido Estacionar", "Curva Acentuada a Esquerda", "Proibido Ultrapassar","Sentido Proibid"},
+                1
+        ));
+        // Pergunta placa de Proibido Ultrapassar
+        perguntas.add(new Pergunta(
+                new String[]{"Proibido Estacionar", "Sentido Proibido", "Proibido Estacionar","Proibido Ultrapassar"},
+                3
+        ));
+        // Pergunta da placa de Velocidade Máxima
+        perguntas.add(new Pergunta(
+                new String[]{"Proibido Ultrapassar", "Proibido Estacionar", "Curva Acentuada a Esquerda","Velocidade Máxima"},
+                3
+        ));
+        // Pergunta da placa de Sentido Proíbido
+        perguntas.add(new Pergunta(
+                new String[]{"Sentido proíbido", "Sentido Estacionar", "Velocidade Máxima","Proibido Ultrapassar"},
+                0
+        ));
     }
 
     public void setBtnProximoLayout(View view) {
